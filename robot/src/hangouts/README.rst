6a0541078222bde5b6f9d23c562358c753299656
David Thompson <dthompson@compete.com>
2012-09-18 21:35:13 -0400

    robot: initial commit of selenium hangout bootstrap

---
+++ robot/src/hangouts/README.rst
@@ -0,0 +1,8 @@
+Requirements
+============
+
+ * Install 'chromium-browser' package via apt-get or equivalent
+ * Install google voice and video plugin
+ * pip install selenium
+ * Download ChromeDriver binary for selenium, https://code.google.com/p/chromedriver/
+   and make sure directory with 'chromedriver' binary is in PATH
---
+++ robot/src/hangouts/hangouts.py
@@ -0,0 +1,62 @@
+from itertools import count
+
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+WAIT_FOR_ELEMENT_SECS = 60
+
+HANGOUT_BASE_URL = 'https://plus.google.com/hangouts/_'
+LOGIN_URL = 'https://accounts.google.com/Login'
+
+LOGIN_FIELDS = {
+    'username': 'Email',
+    'password': 'Passwd'
+}
+JOIN_BUTTON_TEXT = 'Join'
+
+# TODO move to config.py
+USERNAME = 'telepresent001'
+PASSWORD = 'selenium'
+
+class HangoutSession(object):
+    def __init__(self, hangout_url, username=USERNAME, password=PASSWORD):
+        self.hangout_url = hangout_url
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(WAIT_FOR_ELEMENT_SECS)
+        if not self._is_logged_in:
+            self._login(username, password)
+        self._join_hangout(hangout_url)
+
+    def _join_hangout(self, url):
+        if HANGOUT_BASE_URL not in url:
+            raise ValueError("Invalid google hangout URL.")
+        self.driver.get(url)
+        # TODO this is silly and slow as hell.
+        try:
+            for i in count():
+                if i > 10:
+                    raise NoSuchElementException
+                buttons = self.driver.find_elements_by_css_selector('div[role="button"]')
+                try:
+                    button = [b for b in buttons if JOIN_BUTTON_TEXT in b.text][0]
+                    break
+                except IndexError:
+                    pass
+        except NoSuchElementException:
+            raise NoSuchElementException("Could not locate the join-hangout button.")
+        button.click()
+
+    def _login(self, username, password):
+        self.driver.get(LOGIN_URL)
+        self.driver.find_element_by_id(LOGIN_FIELDS['username']).send_keys(username)
+        el = self.driver.find_element_by_id(LOGIN_FIELDS['password'])
+        el.send_keys(password)
+        el.submit()
+
+    @property
+    def _is_logged_in(self):
+        cookies = [(c['domain'], c['name']) for c in self.driver.get_cookies()]
+        if ('.google.com', 'SSID') in cookies:
+            return True
+        else:
+            return False
