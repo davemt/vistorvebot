cc6d7b0d68e8afa17cf35b2fe9759da14d43684d
Steven Willis <onlynone@gmail.com>
2012-09-18 22:39:17 -0400

    controlserver: really simple web socket handler

--- robot/src/controlserver/control_wsh.py
+++ robot/src/controlserver/control_wsh.py
@@ -9,8 +9,7 @@ For example, if the resource name is /example/chat, the handler defined in
 <websock_handlers>/example/chat_wsh.py is invoked.
 """
 
-
-def web_socket_do_extra_handshare(request):
+def web_socket_do_extra_handshake(request):
     """web_socket_do_extra_handshake is called during the handshake after the
     headers are successfully parsed and WebSocket properties (ws_location,
     ws_origin, and ws_resource) are added to request. A handler
@@ -34,8 +33,9 @@ def web_socket_do_extra_handshare(request):
     None, you must choose one subprotocol from this list and set it to
     ws_protocol.
     """
+    print "incoming handshake"
 
-    pass
+
 
 def web_socket_transfer_data(request):
     """web_socket_transfer_data is called after the handshake completed
@@ -66,8 +66,15 @@ def web_socket_transfer_data(request):
     for closing handshake acknowledgement coming from the client. When it
     couldn't receive a valid acknowledgement, raises an exception.
     """
-
-    pass
+    while True:
+        line = request.ws_stream.receive_message()
+        if line is None:
+            return
+        if isinstance(line, unicode):
+            request.ws_stream.send_message(line, binary=False)
+            print "echo: " + line
+        else:
+            request.ws_stream.send_message(line, binary=True)
 
 def web_socket_passive_closing_handshake(request):
     """web_socket_passive_closing_handshake is called after the server receives
@@ -82,4 +89,5 @@ def web_socket_passive_closing_handshake(request):
     standalone.py) is configured to use threads.
     """
 
-    pass
+    print "closing handshake"
+
